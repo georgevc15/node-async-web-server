a092389a58282bad3eec910c6eba25fd26bb6144
George Codat Tudoran <office@solcreation.ro>
2016-07-05 22:01:14 +0300

    changes to the async node server

--- routes/pet.js
+++ routes/pet.js
@@ -4,6 +4,19 @@ var r = require('request').defaults({
 
 var async = require('async');
 
+
+var port = process.env.PORT || 3002;
+console.log("Variabila port din routes "+port);
+
+   if(port == 3002) {
+    var catServerLink = 'http://localhost:3000/cat';
+    var dogServerLink = 'http://localhost:3001/dog';
+   } else {
+    var catServerLink = 'https://cryptic-lowlands-67233.herokuapp.com/cat';
+    var dogServerLink = 'https://cryptic-lowlands-67233.herokuapp.com/dog';
+   }
+
+
 module.exports = function(app) {
 
     /* Read */
@@ -11,7 +24,7 @@ module.exports = function(app) {
 
         async.parallel({
             cat: function(callback){
-                r({uri: 'http://localhost:3000/cat'}, function(error, response, body) {
+                r({uri: catServerLink}, function(error, response, body) {
                     if (error) {
                         callback({service: 'cat', error: error});
                         return;
@@ -24,7 +37,7 @@ module.exports = function(app) {
                 });
             },
             dog: function(callback){
-                r({uri: 'http://localhost:3001/dog'}, function(error, response, body) {
+                r({uri: dogServerLink}, function(error, response, body) {
                     if (error) {
                         callback({service: 'dog', error: error});
                         return;
