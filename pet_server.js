18e019e03f2324d40584ac13756610891f9386f9
George Codat Tudoran <office@solcreation.ro>
2016-07-05 21:47:14 +0300

    main server can run in production mode

--- pet_server.js
+++ pet_server.js
@@ -11,7 +11,15 @@ var petRoutes = require('./routes/pet.js')(app);
 
 
 
-
+/*
 var server = app.listen(3002, function () {
     console.log('Server running at http://127.0.0.1:3002/');
+});
+*/
+
+
+var port = process.env.PORT || 3002;
+
+var server = app.listen(port, function() {
+	console.log("Server running on port "+port);
 });
